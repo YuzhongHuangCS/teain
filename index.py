069e329e2af61b3d6e98632777ba08433b48ed9a
Amrzs <amrzszhang@gmail.com>
2014-04-30 08:42:43 -0700

    test first

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ def app(environ, start_response):
     status = '200 OK'
     headers = [('Content-type', 'text/html')]
     start_response(status, headers)
-    body=["Welcome to Baidu Cloud!\n"]
+    body=["Welcome to \n"]
     return body
 
 from bae.core.wsgi import WSGIApplication
