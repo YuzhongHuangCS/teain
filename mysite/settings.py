3cdb06996974a51f141a89daee2f870b5d8bb8bc
Amrzs <amrzszhang@gmail.com>
2014-05-03 06:31:20 -0700

    new django 1.5.2

--- mysite/settings.py
+++ mysite/settings.py
@@ -83,7 +83,7 @@ STATICFILES_FINDERS = (
 )
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = '&6sje^a-95hf89i$x4qvna=jr&uw^q5qwkivvkt%#gf54d915h'
+SECRET_KEY = '@q(4q6k^pka3=iu##8tjw9z(2jjyf+dxuq@y)ibwsd93!ek*qy'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
