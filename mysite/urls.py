c75ce68068d6fdc8b622791703ca4699e5fc19ac
dmxcsnsbh <l.dmxcsnsbh@gmail.com>
2014-05-03 22:05:40 +0800

    Correct

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,7 +14,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-    url(r'^tea/', 'index'),
+    url(r'^tea/', 'tee.views.index'),
 )
 
 
