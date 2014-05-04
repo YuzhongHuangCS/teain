9e1617b1b13588b3a8bfbd15805ec7b139f55aa5
dmxcsnsbh <l.dmxcsnsbh@gmail.com>
2014-05-04 23:24:14 +0800

    mysql

--- mysite/settings.py
+++ mysite/settings.py
@@ -16,7 +16,7 @@ DATABASES = {
         # The following settings are not used with sqlite3:
         'USER': 'tea',
         'PASSWORD': 'tea-in',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
 }
