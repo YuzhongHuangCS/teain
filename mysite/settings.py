bd1fa23277d4b72fa1f3b980109b3738b105e068
dmxcsnsbh <l.dmxcsnsbh@gmail.com>
2014-05-05 21:41:40 +0800

    database

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,12 +12,12 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'tea_database',                      # Or path to database file if using sqlite3.
+        'NAME': 'LzrGNBKhJYocWuLpjCou',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'tea',
-        'PASSWORD': 'tea-in',
-        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'USER': 'sewaGcLNm8GaU385SZsi9q5a',
+        'PASSWORD': 'GFAMRoqKDVIScFP2GQFoRd8aIaQsPNhf',
+        'HOST': 'sqld.duapp.com',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '4050',                      # Set to empty string for default.
     }
 }
 
