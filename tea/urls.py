e672537f0871dad5b9b7369a8d40adf8cb687b3d
Amrzs <amrzszhang@gmail.com>
2014-05-20 22:50:54 +0800

    user log, but doesn't done

--- tea/urls.py
+++ tea/urls.py
@@ -8,7 +8,12 @@ urlpatterns = patterns('',
     url(r'^get_cloth/(\d+)/$', views.get_cloth, name='cloth'),
     url(r'^get_clothes/(\d+)/(\d+)/$', views.get_clothes, name='clothes'),
     url(r'^get_cloth_imgs/(\d+)/$', views.get_cloth_imgs, name='cloth_imgs'),
+    url(r'^get_cloth_sizes/(\d+)/$', views.get_cloth_sizes, name='cloth_sizes'),
 
+    # user log
+    url(r'^register/$', views.register, name='register'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^logout/$', views.logout, name='logout'),
 
     # test view pages
     url(r'^cloth_show/(\d+)/$', views.cloth_show),
--- tea/views.py
+++ tea/views.py
@@ -1,10 +1,11 @@
 #coding=utf-8
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from django.core import serializers
 
-from tea.models import Cloth, ClothImg
+from tea.models import Cloth, ClothImg, ClothSize
 # Create your views here.
 
 def get_cloth(request, cloth_id):
@@ -26,6 +27,12 @@ def get_cloth_imgs(request, cloth_id):
     data = serializers.serialize('json', cloth_imgs)
     return HttpResponse(data)
 
+def get_cloth_sizes(request, cloth_id):
+
+    cloth_sizes = ClothSize.objects.filter(cloth=cloth_id)
+    data = serializers.serialize('json', cloth_sizes)
+    return HttpResponse(data)
+
 #-----------------------------------------------------------
 
 def cloth_show(request, cloth_id):
@@ -45,3 +52,40 @@ def cloth_imgs_show(request, cloth_id):
         'cloth_imgs': cloth_imgs,
     }
     return render(request, 'tea/show_imgs.html', cloth_dict)
+
+# user register
+#--------------------------------------------------------------------
+
+def register(request):
+
+    if 'POST' == request.method:
+        username = request.POST['username']
+        password = request.POST['password']
+        email = request.POST['email']
+        user = User.objects.create_user(username, email, password)
+        if user is not None:
+            user.save()
+            data = serializers.serialize('json', {'msg': 'ok'})
+            return HttpResponse(data)
+        else:
+            data = serializers.serialize('json', {'msg': 'failed'})
+            return HttpResponse(data)
+
+
+def login_user(request):
+
+    if 'POST' == request.method:
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                # return HttpResponseRedirect()
+            else:
+                pass
+
+
+def logout_user(request):
+
+    logout(request)
