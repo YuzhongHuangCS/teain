d3fb8f39163c66e00417e7eb8b20fbd133b86c2f
PillowSky <ihyzi@163.com>
2014-05-30 16:52:24 +0800

    ugly scroll load implement

--- js/controllers.js
+++ js/controllers.js
@@ -39,6 +39,81 @@ teaControllers.controller('loginController', ['$scope', '$http',
 
 teaControllers.controller('flowController', ['$scope', 'tshirt',
     function($scope, tshirt) {
-        $scope.clothes = tshirt.flow({start: 1, limit: 9});
+
+        /*function loadPage(pageID) {
+            window.pages = pageID;
+            var container = document.querySelector('#flow');
+            var block = document.createElement('div')
+            block.setAttribute('class', 'block');
+            block.setAttribute('ng-repeat', 'cloth in clothes');
+            block.setAttribute('ng-include', "'partials/tea-flow-block.html'");
+            container.appendChild(block);
+            setTimeout(function() {
+                $scope.clothes = tshirt.flow({
+                    start: 1,
+                    limit: 6
+                });
+                $scope.$apply();
+            }, 1000);
+        }
+        loadPage(1);*/
+
+        function loadPage(pageID) {
+            switch (pageID) {
+                case 1:
+                    $scope.clothes1 = tshirt.flow({
+                        start: pageID * 6 - 5,
+                        limit: 6
+                    });
+                    break;
+
+                case 2:
+                    $scope.clothes2 = tshirt.flow({
+                        start: pageID * 6 - 5,
+                        limit: 6
+                    });
+                    break;
+
+                case 3:
+                    $scope.clothes3 = tshirt.flow({
+                        start: pageID * 6 - 5,
+                        limit: 6
+                    });
+                    break;
+
+                case 4:
+                    $scope.clothes4 = tshirt.flow({
+                        start: pageID * 6 - 5,
+                        limit: 6
+                    });
+                    break;
+
+                case 5:
+                    $scope.clothes5 = tshirt.flow({
+                        start: pageID * 6 - 5,
+                        limit: 6
+                    });
+                    break;
+
+                case 6:
+                    $scope.clothes6 = tshirt.flow({
+                        start: pageID * 6 - 5,
+                        limit: 6
+                    });
+                    break;
+            }
+
+        }
+        window.pages = 1;
+        loadPage(window.pages);
+        window.onscroll = function(event) {
+            if ((window.pages * window.innerHeight - window.scrollY) < 0) {
+                //alert(window.pages);
+                if (window.pages <= 7) {
+                    loadPage(++window.pages)
+                }
+
+            }
+        }
     }
 ]);
